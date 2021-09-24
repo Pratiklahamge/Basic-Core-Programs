e56d617baf270e8f32025e4e84899493fec674ec
pratikl <plahamge@gmail.com>
2021-09-24 22:36:07 +0530

    Evenorodd.java

---
+++ Evenorodd.java
@@ -0,0 +1,21 @@
+package Com.Bridgelabz;
+
+import java.util.Scanner;
+
+public class Evenorodd {
+
+	public static void main(String[] args) {
+		 @SuppressWarnings("resource")
+		Scanner reader = new Scanner(System.in);
+
+	        System.out.print("Enter a number: ");
+	        int num = reader.nextInt();
+
+	        if(num % 2 == 0)
+	            System.out.println(num + " is even");
+	        else
+	            System.out.println(num + " is odd");
+
+	}
+
+}
