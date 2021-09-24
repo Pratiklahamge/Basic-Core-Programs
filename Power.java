8269e27d16b6a4ec80d526bcb2a3158fce556412
pratikl <plahamge@gmail.com>
2021-09-24 21:56:37 +0530

    Powerof2

---
+++ Power.java
@@ -0,0 +1,23 @@
+package Com.Bridgelabz;
+
+public class Power {
+
+    static boolean isPowerOfTwo(int n)
+    {
+        return (int)(Math.ceil((Math.log(n) / Math.log(2))))
+            == (int)(Math.floor(((Math.log(n) / Math.log(2)))));
+    }
+ 
+   
+    public static void main(String[] args)
+    {
+        if (isPowerOfTwo(31))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+ 
+        if (isPowerOfTwo(64))
+            System.out.println("leapyear");
+        else
+            System.out.println("Not leapyear");
+    }}
